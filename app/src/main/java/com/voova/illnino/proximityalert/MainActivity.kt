c05af576507d8430b1997d8ef878309743fc2ac5
No_Voovagroup <no@voovagroup.com>
2018-05-09 12:39:04 +0700

    Add Dwell and check Entering and Exiting point accuracy

--- app/src/main/java/com/voova/illnino/proximityalert/MainActivity.kt
+++ app/src/main/java/com/voova/illnino/proximityalert/MainActivity.kt
@@ -11,19 +11,37 @@ import android.location.*
 import android.location.GpsStatus.GPS_EVENT_STARTED
 import android.location.GpsStatus.GPS_EVENT_STOPPED
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Toast
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_main.*
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     companion object {
         lateinit var getInstance: MainActivity
         val ProximityAlert = "ProximityAlert"
+
+        var proximityBundle = Bundle()
+        lateinit var proximityPoint: ProximityPoint
+        var key: String = ""
+        var proximityEnteringExitingStatus: Boolean = false
+
+        var lastestPointName: String = ""
+        var lastesEnteringExitingStatus:Boolean = false
+        var actualEntering:Boolean = false
+        var actualExit:Boolean = false
+        var actualStatusCheck:Int = 0
+        var isCalculating:Boolean = false
     }
     val TAG: String = "MainActivity"
     lateinit var locationManager: LocationManager
@@ -33,6 +51,7 @@ class MainActivity : AppCompatActivity() {
     val locationListener: LocationListener = object : LocationListener {
         override fun onLocationChanged(location: Location) {
             updateDisplayView("onLocationChanged: ${location?.latitude}, ${location?.longitude}")
+            updateDisplayView("accuracy: ${location?.accuracy} meters ")
         }
         override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
             for (i in extras.keySet()){
@@ -77,10 +96,10 @@ class MainActivity : AppCompatActivity() {
                     locationListener)
 
             proximities = ArrayList()
-            val firstPoint = ProximityPoint(996, 12.880350, 100.895056, 15F, "Voova")
-            val secondPoint = ProximityPoint(997, 12.879973, 100.894314, 15F, "Sea max condo")
-            val thirdPoint = ProximityPoint(998, 12.880558, 100.896136, 15F, "Aunty's restaurant")
-            val fourthPoint = ProximityPoint(999, 12.877134, 100.885354, 15F, "Beach Police Booth Chaiyaphruek Rd")
+            val firstPoint = ProximityPoint(996, 12.880355, 100.895128, 25F, "Voova")
+            val secondPoint = ProximityPoint(997, 12.879973, 100.894314, 25F, "Sea max condo")
+            val thirdPoint = ProximityPoint(998, 12.880558, 100.896136, 25F, "Aunty's restaurant")
+            val fourthPoint = ProximityPoint(999, 12.877134, 100.885354, 25F, "Beach Police Booth Chaiyaphruek Rd")
 
             proximities.add(firstPoint)
             proximities.add(secondPoint)
@@ -97,7 +116,9 @@ class MainActivity : AppCompatActivity() {
     fun updateDisplayView(txt: String){
         val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
         val currentDate = sdf.format(Date())
-        tv_display_status.append("$currentDate: $txt\n")
+        runOnUiThread {
+            tv_display_status.append("$currentDate: $txt\n")
+        }
         //sv_display.fullScroll(View.FOCUS_DOWN)
     }
 
@@ -105,9 +126,10 @@ class MainActivity : AppCompatActivity() {
     fun addProximity(proxList: ArrayList<ProximityPoint>){
         for (prox in proxList){
             updateDisplayView("Add proximity point : ${prox.name}, ${prox.lat}, ${prox.lon}, radius: ${prox.radius}")
-            val intent:Intent = Intent(ProximityAlert).apply {
-                putExtra("name", prox.name)
-            }
+            val intent = Intent(ProximityAlert)
+            var bundle = Bundle()
+            bundle.putParcelable("proximity_point", prox)
+            intent.putExtra("proximity_data", bundle)
 
             val pendingIntent: PendingIntent = PendingIntent.getBroadcast(this, prox.id, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
@@ -122,28 +144,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun removeProximity(){
-
         for (prox in proximities){
             Log.d(TAG, "removeProximity id: ${prox.id}")
             val intent = Intent(ProximityAlert)
             val pendingIntent = PendingIntent.getBroadcast(this , prox.id, intent, 0)
             locationManager?.removeProximityAlert(pendingIntent)
         }
-
     }
 
     fun removeRequestLocationUpdate(){
         Log.d(TAG, "removeRequestLocationUpdate")
         locationManager.removeUpdates(locationListener)
     }
-
-    data class ProximityPoint(
-            var id: Int,
-            var lat: Double,
-            var lon: Double,
-            var radius: Float,
-            var name: String
-    )
 
     override fun onDestroy() {
         removeProximity()
@@ -156,28 +168,134 @@ class MainActivity : AppCompatActivity() {
         val TAG: String = "GeofenceReceiver"
         override fun onReceive(context: Context?, intent: Intent) {
             Log.d(TAG, "Fire !!")
-            MainActivity.getInstance.updateDisplayView("$TAG: Fire")
+            MainActivity.getInstance.updateDisplayView("$TAG: Fire !!")
             for (key: String in  intent.extras.keySet()) {
                 Log.d(TAG, intent.extras[key].toString())
                 MainActivity.getInstance.updateDisplayView("Intent $key: ${intent.extras[key]}")
             }
 
+            proximityBundle = intent.extras["proximity_data"] as Bundle
+            proximityPoint = proximityBundle.getParcelable<ProximityPoint>("proximity_point")
+
+            MainActivity.getInstance.updateDisplayView("Incoming point: ${proximityPoint.name}")
+            MainActivity.getInstance.updateDisplayView("======================================")
+
             if (intent.action.equals(ProximityAlert)){
 
-                val key: String = LocationManager.KEY_PROXIMITY_ENTERING
-                val entering: Boolean = intent.getBooleanExtra(key, false)
+                key = LocationManager.KEY_PROXIMITY_ENTERING
+                proximityEnteringExitingStatus = intent.getBooleanExtra(key, false)
 
-                if (entering){
-                    val m = "\n======\nYou are Entering ${intent.extras["name"]} !!\n======"
-                    Log.d(TAG, m)
-                    MainActivity.getInstance.updateDisplayView(m)
-                    Toast.makeText(context, m, Toast.LENGTH_LONG).show()
-                }else{
-                    val m = "\n======\nYou are Exiting ${intent.extras["name"]} !!\n======"
-                    Log.d(TAG, m)
-                    MainActivity.getInstance.updateDisplayView(m)
-                    Toast.makeText(context, m, Toast.LENGTH_LONG).show()
+                if (!isCalculating) {
+
+                    isCalculating = true
+
+                    if (!lastestPointName.equals(proximityPoint.name, true)){
+                        lastestPointName = proximityPoint.name
+                    }
+                    if (lastesEnteringExitingStatus != proximityEnteringExitingStatus){
+                        lastesEnteringExitingStatus = proximityEnteringExitingStatus
+                    }
+                    Observable
+                            .just(true)
+                            .subscribeOn(Schedulers.io())
+                            .map {
+
+                                if (lastestPointName.equals(proximityPoint.name, true) &&
+                                        lastesEnteringExitingStatus == proximityEnteringExitingStatus) {
+                                    actualStatusCheck++
+                                }
+
+                                MainActivity.getInstance.updateDisplayView("Step 1 \nLastest pointName: $lastestPointName \nNew point: ${proximityPoint.name}, \nActualStatusCheck: $actualStatusCheck")
+                            }
+                            .delay(2, TimeUnit.SECONDS)
+                            .map {
+                                if (lastestPointName.equals(proximityPoint.name, true) &&
+                                        lastesEnteringExitingStatus == proximityEnteringExitingStatus) {
+                                    actualStatusCheck++
+                                }
+
+                                MainActivity.getInstance.updateDisplayView("Step 2 \nLastest pointName: $lastestPointName \nNew point: ${proximityPoint.name}, \nActualStatusCheck: $actualStatusCheck")
+                            }
+                            .delay(2, TimeUnit.SECONDS)
+                            .map {
+                                if (lastestPointName.equals(proximityPoint.name, true) &&
+                                        lastesEnteringExitingStatus == proximityEnteringExitingStatus) {
+                                    actualStatusCheck++
+                                }
+
+                                if (actualStatusCheck >= 3) {
+                                    if (lastesEnteringExitingStatus) {
+                                        actualEntering = true
+                                    } else {
+                                        actualExit = true
+                                    }
+                                }
+
+                                MainActivity.getInstance.updateDisplayView("Step 3 \nLastest pointName: $lastestPointName \nNew point: ${proximityPoint.name}, \nActualStatusCheck: $actualStatusCheck\nlastesStatus: $lastesEnteringExitingStatus\nactualEntering: $actualEntering\nactualExit: $actualExit")
+
+                            }
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe{
+                                MainActivity.getInstance.updateDisplayView("Step 4 \nactualEntering: $actualEntering \nactualExit: $actualExit")
+                                if (actualEntering) {
+                                    val m = "\n======================================\nYou are Entering ${lastestPointName} !!\n======================================"
+                                    Log.d(TAG, m)
+                                    MainActivity.getInstance.updateDisplayView(m)
+                                    Toast.makeText(context, m, Toast.LENGTH_LONG).show()
+                                }
+                                if (actualExit) {
+                                    val m = "\n======================================\nYou are Exiting ${lastestPointName} !!\n======================================"
+                                    Log.d(TAG, m)
+                                    MainActivity.getInstance.updateDisplayView(m)
+                                    Toast.makeText(context, m, Toast.LENGTH_LONG).show()
+                                }
+
+                                lastestPointName = ""
+                                lastesEnteringExitingStatus = false
+                                actualEntering = false
+                                actualExit = false
+                                actualStatusCheck = 0
+                                isCalculating = false
+                            }
                 }
+            }
+        }
+    }
+
+
+
+    data class ProximityPoint(
+            var id: Int,
+            var lat: Double,
+            var lon: Double,
+            var radius: Float,
+            var name: String
+    ) : Parcelable {
+        constructor(source: Parcel) : this(
+                source.readInt(),
+                source.readDouble(),
+                source.readDouble(),
+                source.readFloat(),
+                source.readString()
+        )
+
+        override fun describeContents() = 0
+
+        override fun writeToParcel(dest: Parcel, flags: Int) {
+            with(dest) {
+                writeInt(id)
+                writeDouble(lat)
+                writeDouble(lon)
+                writeFloat(radius)
+                writeString(name)
+            }
+        }
+
+        companion object {
+            @JvmField
+            val CREATOR: Parcelable.Creator<ProximityPoint> = object : Parcelable.Creator<ProximityPoint> {
+                override fun createFromParcel(source: Parcel): ProximityPoint = ProximityPoint(source)
+                override fun newArray(size: Int): Array<ProximityPoint?> = arrayOfNulls(size)
             }
         }
     }
